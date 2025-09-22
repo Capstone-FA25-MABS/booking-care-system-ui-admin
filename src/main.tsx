d8e8fb523c10c89e9d98657aaf1e4cabf3623a90
AtuDk3 <thanhtudzx@gmail.com>
2025-09-22 18:38:48 +0700

    [FIX] conflict import bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -12,16 +12,3 @@ createRoot(document.getElementById('root')!).render(
         <App />
     </StrictMode>
 );
-
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import 'simplebar-react/dist/simplebar.min.css';
-// import './styles/index.scss';
-
-// createRoot(document.getElementById('root')!).render(
-//     <StrictMode>
-//         <App />
-//     </StrictMode>
-// );
