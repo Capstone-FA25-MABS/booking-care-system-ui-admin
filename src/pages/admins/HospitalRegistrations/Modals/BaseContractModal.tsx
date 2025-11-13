61e8604ac778fa3bb5b67c2addbe3b9f20ea08d2
DevHuy44 <kieuvanhuy813@gmail.com>
2025-11-13 21:52:14 +0700

    [FIX] Fix low issues

--- src/pages/admins/HospitalRegistrations/Modals/BaseContractModal.tsx
+++ src/pages/admins/HospitalRegistrations/Modals/BaseContractModal.tsx
@@ -149,9 +149,8 @@ const BaseContractModal: React.FC<BaseContractModalProps> = ({
                                     <>
                                         <span
                                             className="spinner-border spinner-border-sm me-2"
-                                            role="status"
                                             aria-hidden="true"
-                                        ></span>
+                                        ></span>{' '}
                                         Đang xử lý...
                                     </>
                                 ) : (
