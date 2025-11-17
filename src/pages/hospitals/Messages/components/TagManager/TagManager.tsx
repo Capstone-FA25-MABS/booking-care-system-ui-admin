014b635c23b4a862321c2a73ef7ff623448e5958
DinhNgocTam <tamdinh572003@gmail.com>
2025-11-17 20:02:15 +0700

    [FIX] Solve the sonar issues

--- src/pages/hospitals/Messages/components/TagManager/TagManager.tsx
+++ src/pages/hospitals/Messages/components/TagManager/TagManager.tsx
@@ -387,14 +387,13 @@ const TagManager: React.FC<TagManagerProps> = ({
                             aria-label="Đóng hộp thoại"
                             type="button"
                         />
-                        <div
-                            className="modal fade show d-block"
-                            role="dialog"
-                            aria-modal="true"
-                            aria-labelledby="create-tag-modal-title"
-                            style={{ zIndex: 1050 }}
-                        >
-                            <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal fade show d-block" style={{ zIndex: 1050 }}>
+                            <div
+                                className="modal-dialog modal-dialog-centered"
+                                role="dialog"
+                                aria-modal="true"
+                                aria-labelledby="create-tag-modal-title"
+                            >
                                 <div className="modal-content">
                                     <div className="modal-header">
                                         <h5 className="modal-title" id="create-tag-modal-title">
@@ -585,14 +584,13 @@ const TagManager: React.FC<TagManagerProps> = ({
                             aria-label="Đóng hộp thoại"
                             type="button"
                         />
-                        <div
-                            className="modal fade show d-block"
-                            role="dialog"
-                            aria-modal="true"
-                            aria-labelledby="edit-tag-modal-title"
-                            style={{ zIndex: 1050 }}
-                        >
-                            <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal fade show d-block" style={{ zIndex: 1050 }}>
+                            <div
+                                className="modal-dialog modal-dialog-centered"
+                                role="dialog"
+                                aria-modal="true"
+                                aria-labelledby="edit-tag-modal-title"
+                            >
                                 <div className="modal-content">
                                     <div className="modal-header">
                                         <h5 className="modal-title" id="edit-tag-modal-title">
